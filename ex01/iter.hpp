265cf2aa2c214d0ff0f6868c41cbe49a518e665f
matde-je <matidlecosta719@gmail.com>
2024-06-03 11:56:32 +0100

    ex01 done

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -2,14 +2,22 @@
 #define iter_hpp
 
 #include <iostream>
+#include <string>
 
 template <typename T> 
 
+void chars( T a) {
+    std::cout << a << std::endl;
+}
+
 //The third parameter can be an instantiated function template.
 //every element of the array
+template <typename T> 
 void iter(T *array, size_t y, void (*f) (T &))
 {
-    
+    int i = -1;
+    while (++i < (int)y)
+        f(array[i]);
 }
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,5 +1,8 @@
 #include "iter.hpp"
 
 int main(){
-    
+    std::string array[2] = {"ola", "123"};
+    iter(array, 2, chars);
+    int arra[2] = {13, 25};
+    iter(arra,2, chars);
 }
